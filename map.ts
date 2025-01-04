765f766d866d1bb19d0121a69850e8b722008f10
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-04 15:10:31 +0800

    Update map.ts

--- map.ts
+++ map.ts
@@ -1,1 +1,10 @@
-
+import "number"
+export const 1234567890
+export const 2345678901
+export const 3456789012
+export const 4567890123
+export const 6789012345
+export const 7890123456
+export const 8901234567
+export const 9012345678
+export const 0123456789
